49f6327cab60102172d071cce84105f6978200b3
lvsi <lvsi@changba.com>
2022-01-09 23:19:02 +0800

    chore: 完善kafka.容器脚本的启动

--- install/container/kafka.sh
+++ install/container/kafka.sh
@@ -5,12 +5,30 @@ docker pull wurstmeister/zookeeper
 docker pull wurstmeister/kafka
 
 # Start zookeeper.
-docker run -d --name zookeeper -p 2181:2181 -t wurstmeister/zookeeper
+containerCount=0
+for file in $(docker container ls -f name=zookeeperContainer -q)
+do
+    ((containerCount++))
+done
+if [ $containerCount -ne 0 ]; then
+  echo -e "Start zookeeperContainer already, is skipped."
+else
+  docker run -d --name zookeeperContainer -p 2181:2181 -t wurstmeister/zookeeper
+fi
 
 # Start kafka server.
 # use ifconfig command to get the local ip: 192.168.x.x not 127.0.0.1
 localIP=$(ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|grep '192.168' |awk '{print $2}'|tr -d "addr:")
-docker run -d --name kafkaContainer -p 9092:9092 -e KAFKA_BROKER_ID=0 -e KAFKA_ZOOKEEPER_CONNECT=${localIP}:2181 -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://${localIP}:9092 -e KAFKA_LISTENERS=PLAINTEXT://0.0.0.0:9092 -t wurstmeister/kafka
+containerCount=0
+for file in $(docker container ls -f name=kafkaContainer -q)
+do
+    ((containerCount++))
+done
+if [ $containerCount -ne 0 ]; then
+  echo -e "Start kafkaContainer already, is skipped."
+else
+  docker run -d --name kafkaContainer -p 9092:9092 -e KAFKA_BROKER_ID=0 -e KAFKA_ZOOKEEPER_CONNECT=${localIP}:2181 -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://${localIP}:9092 -e KAFKA_LISTENERS=PLAINTEXT://0.0.0.0:9092 -t wurstmeister/kafka
+fi
 
 # Login kafka server container
 # docker exec -it kafkaContainer /bin/bash
