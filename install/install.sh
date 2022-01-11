2638db13d2236bb1cdc39131cc57b520c2f51b83
lvsi <lvsi@changba.com>
2022-01-12 01:30:00 +0800

    feat: 重大更新更容易上手-添加系统变量机制

--- install/install.sh
+++ install/install.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+# Check params.
+if [ ! -n "$1" ] ;then
+    echo -e "Please input the localIP param."
+    exit 1
+else
+    export ESUPDATER_LOCAL_IP=$1
+    systemVariables="ESUPDATER_LOCAL_IP=$1\n"
+    echo -e $systemVariables > ../.env
+fi
+
 # The part of you must do.
 # 1. Make image
 cd image && bash make.sh && cd ..
